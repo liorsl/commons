6632907482e6c772ad8cd908bdc2c0fe739cdf6d
Lior Slakman <liorslakman3@gmail.com>
2020-04-16 05:52:08 +0300

    Removed collections pacakge

--- src/net/apartium/commons/collections/EachList.java
+++
@@ -1,5 +0,0 @@
-package net.apartium.commons.collections;
-
-import java.util.List;
-
-public interface EachList<E> extends List<E>, EachIterable<E> {}
--- src/net/apartium/commons/collections/EachSet.java
+++
@@ -1,5 +0,0 @@
-package net.apartium.commons.collections;
-
-import java.util.Set;
-
-public interface EachSet<E> extends Set<E>, EachIterable<E> {}
